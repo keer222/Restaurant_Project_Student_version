14263c555c31bb776d09baa6c0001c928e1e7291
Keerthana K <keertha222@gmail.com>
2022-04-30 07:52:43 +0530

    All the methods and refactoring completed

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -18,15 +18,14 @@ public class Restaurant {
     }
 
     public boolean isRestaurantOpen() {
-        return true;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+        LocalTime currentTime = getCurrentTime();
+        return currentTime.compareTo(openingTime) > 0 && currentTime.compareTo(closingTime) < 0 ? true : false;
     }
 
     public LocalTime getCurrentTime(){ return  LocalTime.now(); }
 
     public List<Item> getMenu() {
-        return null;
-        //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
+        return menu;
     }
 
     private Item findItemByName(String itemName){
--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -5,9 +5,13 @@ import java.util.List;
 public class RestaurantService {
     private static List<Restaurant> restaurants = new ArrayList<>();
 
-    public Restaurant findRestaurantByName(String restaurantName){
+    public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException {
+        for(Restaurant restaurant:restaurants)
+            if(restaurant.getName().equalsIgnoreCase(restaurantName))
+                return restaurant;
+            else
+                throw new restaurantNotFoundException("Restaurant Not Found!");
         return null;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
     }
 
 
